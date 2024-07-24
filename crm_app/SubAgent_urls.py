4079d54ff9f5b01c642892ee1b5de915590a60e7
armaan <armaanalam65@gmail.com>
2024-07-24 19:43:47 +0530

    new updated

--- crm_app/SubAgent_urls.py
+++ crm_app/SubAgent_urls.py
@@ -4,12 +4,12 @@ urlpatterns = [
 
     path("Dashboard/", subagent_dashboard.as_view(), name="subagent_dashboard"),
     # --------------------------- Enquiry ----------------------------------
-    path("AddEnquiry/", Enquiry1View.as_view(), name="agent_enquiry_form1"),
-    path("AddEnquiry2/", Enquiry2View.as_view(), name="agent_enquiry_form2"),
-    path("AddEnquiry3/", Enquiry3View.as_view(), name="agent_enquiry_form3"),
-    # path("enquiry_form4/<int:id>/", agentdocument, name="agent_enquiry_form4"),
-    # path("Uploaddocument/", upload_document, name="agent_uploaddocument"),
-    # path("Delete/UploadFile/<int:id>", delete_docfile, name="agent_docfile"),
+    path("AddEnquiry/", Enquiry1View.as_view(), name="subagent_enquiry_form1"),
+    path("AddEnquiry2/", Enquiry2View.as_view(), name="subagent_enquiry_form2"),
+    path("AddEnquiry3/", Enquiry3View.as_view(), name="subagent_enquiry_form3"),
+    # path("enquiry_form4/<int:id>/", agentdocument, name="subagent_enquiry_form4"),
+    # path("Uploaddocument/", upload_document, name="subagent_uploaddocument"),
+    # path("Delete/UploadFile/<int:id>", delete_docfile, name="subagent_docfile"),
 
 
     path("AllNewLeads/", subagent_new_leads_details, name="subagent_new_leads_details"),
